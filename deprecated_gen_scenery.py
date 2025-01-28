f6206782e61358cd10c3d542026d10093cd9fff6
Austin Wagner <austinwagner@msn.com>
2025-01-28 05:23:07 -0800

    Removed code from gen_scenery

---
+++ deprecated_gen_scenery.py
@@ -0,0 +1,14 @@
+# FileName: gen_scenery.py
+# version: 1.0
+# Summary: Consolidates all logic for placing procedural scenery: rivers, grass, rocks, trees, bridges, etc.
+# Tags: map, generation, scenery
+
+"""
+All scenery-related functions have been moved into separate files:
+  - gen_rivers.py      (rivers)
+  - gen_grass.py       (grass patches, BFS, semicircle grass, etc.)
+  - gen_rocks.py       (rocks)
+  - gen_trees.py       (trees)
+  - gen_bridges.py     (bridging)
+This file is kept only for reference and is now effectively empty.
+"""
